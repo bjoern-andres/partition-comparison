579dbde339b5b674ecf42471aee0cca5a54c494b
Thouis (Ray) Jones <thouis@seas.harvard.edu>
2012-12-07 22:39:57 -0500

    Add rand_index, and make type sizes more explicit

--- src/partition_comparison.pyx
+++ src/partition_comparison.pyx
@@ -1,17 +1,25 @@
 cimport cython
 
+from libc.stdint cimport int8_t, int16_t, int32_t, int64_t
+from libc.stdint cimport uint8_t, uint16_t, uint32_t, uint64_t
+
 cdef extern from "andres/partition-comparison.hxx" namespace "andres":
+   double randIndex(...)
    double variationOfInformation(...)
 
 ctypedef fused Label:
-    cython.char
-    cython.uchar
-    cython.short
-    cython.ushort
-    cython.long
-    cython.ulong
-    cython.longlong
-    cython.ulonglong
+    int8_t
+    int16_t
+    int32_t
+    int64_t
+    uint8_t
+    uint16_t
+    uint32_t
+    uint64_t
+
+def rand_index(Label[::1] x, Label[::1] y):
+    assert x.size == y.size
+    return randIndex(&(x[0]), (&x[0]) + <int> x.size, &(y[0]), True)
 
 def variation_of_information(Label[::1] x, Label[::1] y):
     assert x.size == y.size
